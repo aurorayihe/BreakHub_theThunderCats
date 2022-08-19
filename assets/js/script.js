86718c4c7febe15ddd23ad20a08498d99463f4b0
Nunder <nathannunderwood@gmail.com>
2022-08-18 19:46:03 -0700

    Merge random cat generator css and js with Main

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,3 +48,51 @@ function gifInit() {
   });
 }
 
+// Random cat image and fact generator - Nate
+var url = `https://api.thecatapi.com/v1/breeds`;
+var api_key = "a747f151-8a97-4d37-8633-646029636f01"
+var storedBreeds = []
+
+ fetch(url,{headers: {
+      'x-api-key': api_key
+    }})
+ .then((response) => {
+   return response.json();
+ })
+.then((data) => {
+   
+   //filter to only include those with an `image` object
+   data = data.filter(img=> img.image?.url!=null)
+   
+  storedBreeds = data;
+   
+   for (var i = 0; i < storedBreeds.length; i++) {
+    var breed = storedBreeds[i];
+    var option = document.createElement('option');
+     
+     //skip any breeds that don't have an image
+     if(!breed.image)continue
+     
+    //use the current array index
+    option.value = i;
+    option.innerHTML = `${breed.name}`;
+document.getElementById('breed_selector').appendChild(option);
+    
+    }
+   //show the first breed by default
+   showBreedImage(0)
+})
+.catch(function(error) {
+   console.log(error);
+});
+
+function showBreedImage(index)
+{ 
+  document.getElementById("breed_image").src= storedBreeds[index].image.url;
+  
+  document.getElementById("breed_json").textContent= storedBreeds[index].temperament
+  
+  
+  document.getElementById("wiki_link").href= storedBreeds[index].wikipedia_url
+  document.getElementById("wiki_link").innerHTML= storedBreeds[index].wikipedia_url
+}
